1b1860823adaa8b3ae52366a3e7d84ff1cfa993a
jonathan <jonathan.jonsson@decerno.se>
2023-08-30 20:05:41 +0200

    Fixat dbcontext

--- AllwinAPI/AllwinAPI/Model/Interfaces/IStop.cs
+++ AllwinAPI/AllwinAPI/Model/Interfaces/IStop.cs
@@ -4,8 +4,8 @@
     {
         int StopId { get; set; }
         int RouteId { get; set; }
-        double Latitude { get; set; }
-        double Longitude { get; set; }
+        float Latitude { get; set; }
+        float Longitude { get; set; }
         string Name { get; set; }
         string Adress { get; set; }
     }
