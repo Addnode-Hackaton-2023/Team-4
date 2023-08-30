968cc2588ec6ce6a6f11d31cfa57869198a96814
jonathan <jonathan.jonsson@decerno.se>
2023-08-30 12:12:08 +0200

    Example data for stops

--- AllwinAPI/AllwinAPI/Controllers/StopController.cs
+++ AllwinAPI/AllwinAPI/Controllers/StopController.cs
@@ -17,19 +17,34 @@ namespace AllwinAPI.Controllers
                 {
                     routeId = 1,
                     stopId = 1,
-                    name = "Ica nära"
+                    name = "Ica nära",
+                    adress = "Stockholmsvägen 1, 182 78 Stocksund",
+                    Latitude = 0,
+                    Longitude = 0,
+                    ContactPerson = "Person 1",
+                    ContactPhone = "07012345678"
                 },
                 new StopDO()
                 {
                     routeId = 1,
                     stopId = 2,
-                    name = "Coop"
+                    name = "Coop",
+                    adress = "Stockholmsvägen 20, 181 50 Lidingö",
+                    Latitude = 1,
+                    Longitude = 0,
+                    ContactPerson = "Person 2",
+                    ContactPhone = "07012345679"
                 },
                 new StopDO()
                 {
                     routeId = 1,
                     stopId = 3,
-                    name = "Lidl"
+                    name = "Lidl",
+                    adress = "Enhagsvägen 24, 183 34 Täby",
+                    Latitude = 1,
+                    Longitude = 1,
+                    ContactPerson = "Person 3",
+                    ContactPhone = "07012345670"
                 }
             };
         }
--- AllwinAPI/AllwinAPI/Model/StopDO.cs
+++ AllwinAPI/AllwinAPI/Model/StopDO.cs
@@ -2,8 +2,13 @@
 {
     public class StopDO
     {
-        public int routeId;
-        public int stopId;
-        public string name;
+        public int routeId { get; set; }
+        public int stopId { get; set; }
+        public string name { get; set; }
+        public string adress { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string ContactPerson { get; set; }
+        public string ContactPhone { get; set; }
     }
 }
