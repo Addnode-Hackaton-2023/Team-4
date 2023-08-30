8ed052069ca24420daba1fc00b1d28097a22a699
Oskar K <oskar.korsback@outlook.com>
2023-08-30 15:47:29 +0200

    add missing file

--- AllwinAPI/AllwinAPI/Model/StopListDO.cs
+++ AllwinAPI/AllwinAPI/Model/StopListDO.cs
@@ -1,11 +1,12 @@
 ﻿namespace AllwinAPI.Model
 {
-    public class ListStopDO : IStop
+    public class StopListDO : IStop
     {
         public int StopId { get; set; }
         public int RouteId { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public string Name { get;set; } = string.Empty;
         public string Name { get;set; } = string.Empty;
         public string Adress { get; set; } = string.Empty;
     }
