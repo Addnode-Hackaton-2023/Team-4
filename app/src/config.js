967e5af14e8c1e00ff51be7a873699d820389955
Anton Lundholm <anton.lundholm@Sokigo.com>
2023-09-01 07:16:02 +0200

    removed apiKey

--- app/src/config.js
+++ app/src/config.js
@@ -1,4 +1,3 @@
 import esriConfig from '@arcgis/core/config.js'
 
-esriConfig.apiKey =
-    'AAPKaaa696cbe9f44021ba8aa1d271924f71fYD65N2risPRaS7wEFOGmw0ihhMP6Lfo1LxShaM34iX7eKua86RoOAbp0nnFvudi'
+esriConfig.apiKey = '[insert_key_here]'
